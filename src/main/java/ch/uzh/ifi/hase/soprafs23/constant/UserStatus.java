633750d7da8b17becb07e81d11766cf21f2bcbe4
Songlin Wang <Songlin.Wang@uzh.ch>
2023-03-25 02:55:16 +0100

    first commit

--- src/main/java/ch/uzh/ifi/hase/soprafs23/constant/UserStatus.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/constant/UserStatus.java
@@ -1,5 +1,15 @@
 package ch.uzh.ifi.hase.soprafs23.constant;
 
 public enum UserStatus {
-  ONLINE, OFFLINE;
+    ONLINE("online"), OFFLINE("offline");
+
+    private final String status;
+
+    UserStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus(){
+        return status;
+    }
 }
