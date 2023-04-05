607bd5225a029aaecb7a39186cc0c552c511bf0a
fengjiao <fengjiao.ji@uzh.ch>
2023-04-05 23:12:22 +0200

    add StompPrincipal and PrincipalHandshake

--- src/main/java/ch/uzh/ifi/hase/soprafs23/websocket/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/websocket/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package ch.uzh.ifi.hase.soprafs23.websocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -22,6 +25,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
                 .setAllowedOriginPatterns("*")
+                .setHandshakeHandler(new PrincipalHandshake())
                 .withSockJS();
     }
+
+    @Bean
+    public TaskScheduler heartBeatScheduler() {
+        return new ThreadPoolTaskScheduler();
+    }
+
 }
