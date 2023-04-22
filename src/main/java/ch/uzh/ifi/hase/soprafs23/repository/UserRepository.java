ec309b8c1dfae484a6482a7c18cd0532906d850c
fengjiao <fengjiao.ji@uzh.ch>
2023-04-22 13:29:04 +0200

    add getLocalUser api

--- src/main/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User findById(long id);
-    User findByPassword(String password);
+    User findByToken(String token);
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapper.java
@@ -21,6 +21,7 @@ public interface DTOMapper {
 
     @Mapping(source = "username", target = "username")
     @Mapping(source = "password", target = "password")
+    @Mapping(source = "token", target = "token")
     User convertUserPostDTOtoEntity(UserPostDTO userPostDTO);
 
     @Mapping(source = "id", target = "id")
