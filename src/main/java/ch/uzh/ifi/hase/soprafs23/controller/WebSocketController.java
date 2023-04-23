7798e7af001b8b2984c17f235ad5b5ff2882552a
yuanli <liyuanofficial@outlook.com>
2023-04-23 10:35:32 +0200

    Add one boardcast after updating score;

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/WebSocketController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/WebSocketController.java
@@ -198,11 +198,12 @@ public class WebSocketController {
      */
     @MessageMapping("/multi/rooms/{roomID}/players/scoreBoard")
     public void updatePlayerScoreBoard(@DestinationVariable int roomID, PlayerScoreBoardDTO playerScoreBoardDTO) {
-        log.info("Room {}: Player score {} is changing.", roomID, playerScoreBoardDTO);
+        log.info("Room {}: Player score {} is changing.", roomID, playerScoreBoardDTO.getScoreBoard().getSystemScore());
         this.gameService.updatePlayerScore(roomID, playerScoreBoardDTO);
 
         Room updatedRoom = this.gameService.findRoomByID(roomID);
         log.info("Updated room with player score {} : " + updatedRoom);
+        this.simpMessagingTemplate.convertAndSend("/topic/multi/rooms/"+roomID+"/info",updatedRoom);
 
     }
 
@@ -215,6 +216,7 @@ public class WebSocketController {
      */
     @MessageMapping("/multi/rooms/{roomID}/players/scores")
     public void getPlayerScoreBoard(@DestinationVariable int roomID) {
+        log.info("Room {} is retrieving players score.", roomID);
         LinkedHashMap<Integer, Player> playerRank =  this.gameService.calculateRanking(roomID);
         this.simpMessagingTemplate.convertAndSend("/topic/multi/rooms/"+roomID+"/scores", playerRank);
     }
